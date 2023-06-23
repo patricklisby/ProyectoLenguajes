0b3352d4669ac1bc1c321f01887ee02eb82e8ed0
JDavidHA <josuehernandezarauz@gmail.com>
2023-06-22 23:41:59 -0600

    CAMBIO DE HEADER "ORIGIN"

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,8 +1,14 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: X-API-KEY, Origen, X-Request-Width, Content-Type, Accept, Access-Control-Request-Method, Authorization');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE, PATCH');
 header('Allow: GET, POST, OPTIONS, PUT, DELETE, PATCH');
+// Permitir solicitudes desde cualquier origen
+header('Access-Control-Allow-Origin: *');
+
+// Permitir los métodos HTTP GET, POST, PUT y DELETE
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE, PATCH');
+
+// Permitir los encabezados de contenido y autorización
+header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Request-Width, Content-Type, Accept, Access-Control-Request-Method, Authorization');
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     die('Metodo no permitido');
 }
